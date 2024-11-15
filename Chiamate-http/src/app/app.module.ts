af07a79fa87b9fabeeca13da7f9b5bc5cfadf775
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-15 11:18:44 +0000

    metodi post e request

--- Chiamate-http/src/app/app.module.ts
+++ Chiamate-http/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
  declarations: [
-   AppComponent
+   AppComponent,
+   FooComponent
  ],
  imports: [
    BrowserModule, HttpClientModule //Aggiungi il modulo http qui 
