6b4aa796bdee4954eb28e6f8c4aa8b1874885a59
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-22 08:40:22 +0000

    sitonerd finito

--- sitonerd/src/app/app.component.ts
+++ sitonerd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sitonerd';
+  searchTerm: string = '';
+
+  constructor(private router: Router) {}
+
+  searchCard(): void {
+    if (this.searchTerm) {
+      this.router.navigate([`/card/${this.searchTerm}`]);
+    }
+  }
 }
--- sitonerd/src/app/app.module.ts
+++ sitonerd/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { CardComponent } from './card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
